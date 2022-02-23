0cf0dcd808e2b290f5ef0a2c0ee24a62cea39a6a
wqn <861873232@qq.com>
2022-02-23 14:17:07 +0800

    SSR渲染 使用getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,37 @@
-import Link from "next/link"
-import React from "react"
-import Image from 'next/image'
-import png from 'assets/images/img.png'
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import png from 'assets/images/img.png';
+import {UAParser} from 'ua-parser-js';
+import {GetServerSideProps, NextPage} from 'next';
 
-function HomePage() {
-  return (
-    <div>Welcome to Next.js!
-      <h2>
-        <Link href="/posts/first_post">
-          <a>click here</a>
-        </Link>
-      </h2>
-      <Image src={png}/>
-    </div>
-  )
+type Props = {
+  browser: {
+    browser:{
+      name:string,
+      version:string,
+      major:string
+    }
+  }
 }
 
-export default HomePage
+const Index: NextPage<Props> = (props) => {
+  const {browser} = props
+  return (
+    <div>
+      <h1>你的浏览器是:{browser.browser.name}</h1>
+    </div>
+  );
+};
+
+export default Index;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const ua = context.req.headers['user-agent'];
+  const result = new UAParser(ua).getResult();
+  return {
+    props: {
+      browser: JSON.parse(JSON.stringify(result))
+    }
+  };
+};
