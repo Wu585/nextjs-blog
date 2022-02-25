420d02e421818d7b42494ee840ad1edad9cf6dd7
Wu <15151851516@163.com>
2022-02-26 01:10:43 +0800

    成功seed数据

--- src/seed.ts
+++ src/seed.ts
@@ -1,6 +1,30 @@
 import 'reflect-metadata';
 import {createConnection} from 'typeorm';
+import {User} from './entity/User';
+import {Post} from './entity/Post';
+import {Comment} from './entity/Comment';
 
 createConnection().then(async connection => {
+  const {manager} = connection;
+  // 创建 User
+  const u1 = new User();
+  u1.username = 'frank';
+  u1.passwordDigest = 'xxx';
+  await manager.save(u1);
+  console.log(u1.id);
+  // 创建 Post
+  const p1 = new Post();
+  p1.title = 'Post 1';
+  p1.content = '我的第一篇博客';
+  p1.author = u1;
+  await manager.save(p1);
+  console.log(p1.id);
+  // 创建 Comment
+  const c1 = new Comment();
+  c1.user = u1;
+  c1.post = p1;
+  c1.content = 'Good Blog';
+  await manager.save(c1);
+  console.log(c1.id);
   await connection.close();
 }).catch(error => console.log(error));
