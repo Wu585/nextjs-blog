0495e67e2f034ae3390adfd9a533cdb88a108770
Wu <15151851516@163.com>
2022-02-26 23:27:29 +0800

    删除log

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import {GetServerSideProps, NextPage} from 'next';
 import {getDatabaseConnection} from '../lib/getDatabaseConnection';
 import {Post} from '../src/entity/Post';
 import Link from 'next/link';
-import axios from 'axios';
 
 type Props = {
   posts: Post[]
@@ -11,13 +10,6 @@ type Props = {
 
 const Index: NextPage<Props> = (props) => {
   const {posts} = props;
-  useEffect(()=>{
-    axios.get('/api/v1/test').then(res=>{
-      console.log('res');
-      console.log(res);
-    })
-  },[])
-
   return (
     <div>
       <h1>文章列表</h1>
