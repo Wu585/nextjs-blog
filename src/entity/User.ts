01d2646a46f19d68c3058efaab288385bb466597
Wu <15151851516@163.com>
2022-02-27 19:25:58 +0800

    进行错误检查及为user添加unique索引

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -9,6 +9,7 @@ import {
 } from 'typeorm';
 import {Post} from './Post';
 import {Comment} from './Comment';
+import {getDatabaseConnection} from '../../lib/getDatabaseConnection';
 
 @Entity('users')
 export class User {
@@ -26,4 +27,41 @@ export class User {
   posts: Post[];
   @OneToMany(type => Comment, comment => comment.user)
   comments: Comment[];
+  errors = {
+    username: [] as string[],
+    password: [] as string[],
+    password_confirmation: [] as string[],
+  };
+  password: string;
+  password_confirmation: string;
+
+  async validate() {
+    if (this.username.trim() === '') {
+      this.errors.username.push('用户名不能为空');
+    }
+    if (!/[a-zA-Z0-9]/.test(this.username.trim())) {
+      this.errors.username.push('格式不合法');
+    }
+    if (this.username.trim().length > 42) {
+      this.errors.username.push('太长');
+    }
+    if (this.username.trim().length <= 3) {
+      this.errors.username.push('太短');
+    }
+    const found = await (await getDatabaseConnection()).manager.find(
+      User, {username: this.username});
+    if (found.length > 0) {
+      this.errors.username.push('已存在，不能重复注册');
+    }
+    if (this.password === '') {
+      this.errors.username.push('密码不能为空');
+    }
+    if (this.password !== this.password_confirmation) {
+      this.errors.password_confirmation.push('密码不匹配');
+    }
+  }
+
+  hasErrors() {
+    return !!Object.values(this.errors).find(v => v.length > 0);
+  }
 }
