7ceada60880aebf77161ac94a9b20c31e0afca1c
Wu <15151851516@163.com>
2022-03-09 22:43:32 +0800

    创建 posts new 页面

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,8 +1,54 @@
 import {NextPage} from 'next';
+import Form from '../../components/Form';
+import {useCallback, useState} from 'react';
+import axios, {AxiosResponse} from 'axios';
 
 const PostsNew: NextPage = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    content: '',
+  });
+  const [errors, setErrors] = useState({
+    title: [],
+    content: [],
+  });
+  const onSubmit = useCallback((e) => {
+    e.preventDefault();
+    axios.post('/api/v1/posts', formData)
+      .then(() => {
+        window.alert('创建博客成功');
+      }, (error) => {
+        if (error.response) {
+          const response: AxiosResponse = error.response;
+          if (response.status === 422) {
+            setErrors(response.data);
+          }
+        }
+      });
+  }, [formData]);
+  const onChange = useCallback((key, value) => {
+    setFormData({...formData, [key]: value});
+  }, [formData]);
   return (
-    <div>PostsNew</div>
+    <Form
+      onSubmit={onSubmit}
+      buttons={
+        <>
+          <button type="submit">
+            提交
+          </button>
+        </>
+      }
+      fields={[{
+        label: '标题', type: 'text', value: formData.title,
+        onChange: e => onChange('username', e.target.value),
+        errors: errors.title
+      },
+        {
+          label: '内容', type: 'textarea', value: formData.content,
+          onChange: e => onChange('password', e.target.value),
+          errors: errors.content
+        }]}/>
   );
 };
 
