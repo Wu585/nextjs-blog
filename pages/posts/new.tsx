5262e3308d4eec4c8dad6afa7f4b448ffa3a2da5
Wu <15151851516@163.com>
2022-04-04 00:10:03 +0800

    封装useForm代替Form

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,19 +1,9 @@
 import {NextPage} from 'next';
-import Form from '../../components/Form';
-import {useCallback, useState} from 'react';
 import axios, {AxiosResponse} from 'axios';
+import {useForm} from '../../hooks/useForm';
 
 const PostsNew: NextPage = () => {
-  const [formData, setFormData] = useState({
-    title: '',
-    content: '',
-  });
-  const [errors, setErrors] = useState({
-    title: [],
-    content: [],
-  });
-  const onSubmit = useCallback((e) => {
-    e.preventDefault();
+  const onSubmit = (formData: typeof initFormData) => {
     axios.post('/api/v1/posts', formData)
       .then(() => {
         window.alert('创建博客成功');
@@ -25,30 +15,23 @@ const PostsNew: NextPage = () => {
           }
         }
       });
-  }, [formData]);
-  const onChange = useCallback((key, value) => {
-    setFormData({...formData, [key]: value});
-  }, [formData]);
+  };
+  const initFormData = {title: '', content: ''};
+  const {form, setErrors} = useForm(
+    {
+      initialFormData: initFormData,
+      fields: [
+        {label: '标题', type: 'text', key: 'title'},
+        {label: '内容', type: 'textarea', key: 'content',}
+      ],
+      buttons: <button type="submit">
+        提交
+      </button>,
+      onSubmit
+    }
+  );
   return (
-    <Form
-      onSubmit={onSubmit}
-      buttons={
-        <>
-          <button type="submit">
-            提交
-          </button>
-        </>
-      }
-      fields={[{
-        label: '标题', type: 'text', value: formData.title,
-        onChange: e => onChange('username', e.target.value),
-        errors: errors.title
-      },
-        {
-          label: '内容', type: 'textarea', value: formData.content,
-          onChange: e => onChange('password', e.target.value),
-          errors: errors.content
-        }]}/>
+    <div>{form}</div>
   );
 };
 
