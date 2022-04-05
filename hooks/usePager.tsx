a5e6fd23213fdc459804b2ba94de09cabf3b2207
Wu <15151851516@163.com>
2022-04-05 14:37:14 +0800

    完成 usePager 组件

---
+++ hooks/usePager.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Link from 'next/link';
+import qs from 'qs';
+import _ from 'lodash';
+
+type Options = {
+  page: number;
+  totalPages: number;
+  urlMaker?: (n: number) => string;
+}
+
+const defaultUrlMaker = (n: number) => `?page=${n}`;
+
+const usePager = (options: Options) => {
+  const {page, totalPages, urlMaker: _urlMaker} = options;
+  const urlMaker = _urlMaker || defaultUrlMaker;
+  const numbers = [];
+  numbers.push(1);
+  for (let i = page - 3; i <= page + 3; i++) {
+    numbers.push(i);
+  }
+  numbers.push(totalPages);
+  const pageNumbers = _.uniq(numbers).sort().filter(n => n > 0 && n <= totalPages).reduce((result, n) => n - (result[result.length - 1] || 0) === 1
+    ? result.concat(n) : result.concat(-1, n), []);
+  const pager = (
+    <div className="wrapper">
+      {page > 1 &&
+      <Link href={urlMaker(page - 1)}>
+        <a>上一页</a>
+      </Link>
+      }
+      {pageNumbers.map(n => n === -1 ? <span key={n}>...</span> :
+        <Link key={n} href={urlMaker(n)}><a>{n}</a></Link>)}
+      {totalPages > page &&
+      <Link href={urlMaker(page + 1)}>
+        <a>下一页</a>
+      </Link>
+      }
+      <span>第 {page} / {totalPages} 页</span>
+      <style jsx>{`
+        .wrapper {
+          margin: 0 -8px;
+        }
+
+        .wrapper > a, .wrapper > span {
+          margin: 0 8px;
+        }
+
+      `}</style>
+    </div>
+  );
+  return {pager};
+};
+
+export default usePager;
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,19 +1,25 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {GetServerSideProps, NextPage} from 'next';
 import {getDatabaseConnection} from 'lib/getDatabaseConnection';
 import {Post} from 'src/entity/Post';
 import Link from 'next/link';
 import qs from 'qs';
+import usePager from '../../hooks/usePager';
 
 type Props = {
   posts: Post[],
   count: number,
   perPageCount: number,
-  page: number
+  page: number,
+  totalPages: number,
 }
 
 const PostsIndex: NextPage<Props> = (props) => {
-  const {posts} = props;
+  const {posts,page,totalPages} = props;
+  const {pager} = usePager({
+    page,
+    totalPages
+  })
   return (
     <div>
       <h1>文章列表</h1>
@@ -27,13 +33,7 @@ const PostsIndex: NextPage<Props> = (props) => {
         </div>
       )}
       <footer>
-        共 {props.count} 篇博客，当前是第 {props.page} 页
-        <Link href={`/posts?${qs.stringify({page: props.page - 1})}`}>
-          <a>上一页</a>
-        </Link>
-        <Link href={`/posts?${qs.stringify({page: props.page + 1})}`}>
-          <a>下一页</a>
-        </Link>
+        {pager}
       </footer>
     </div>
   );
@@ -44,8 +44,8 @@ export default PostsIndex;
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const connect = await getDatabaseConnection();
   const query = qs.parse(context.req.url.split('?')[1]);  // 获取url中的参数
-  const page = query.page ? parseInt(query.page as string) : 1; // 如果没有page参数，默认为1
-  const perPageCount = 3;
+  const page = parseInt(query.page as string) || 1; // 如果没有page参数，默认为1
+  const perPageCount = 1;
   const [posts, count] = await connect.manager.findAndCount(Post, {
     skip: (page - 1) * perPageCount,
     take: perPageCount
@@ -55,7 +55,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       posts: JSON.parse(JSON.stringify(posts)),
       count,
       perPageCount,
-      page
+      page,
+      totalPages: Math.ceil(count / perPageCount)
     }
   };
 };
