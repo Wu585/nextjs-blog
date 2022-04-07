785a251947952b0a283676eb91e3c456b5a3ea56
Wu <15151851516@163.com>
2022-04-07 23:33:20 +0800

    解决connection两次会无法连接的bug

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -1,29 +1,127 @@
 import 'reflect-metadata';
-import {createConnection, getConnectionManager} from 'typeorm';
+import {Connection, getConnectionManager} from 'typeorm';
 import {Post} from 'src/entity/Post';
 import {User} from 'src/entity/User';
 import {Comment} from 'src/entity/Comment';
 import config from 'ormconfig.json';
 
+/*
 const create = async () => {
+  console.log('create');
+
   // @ts-ignore
   return createConnection({
     ...config,
     host: process.env.NODE_ENV === 'production' ? 'localhost' : config.host,
-    database: process.env.NODE_ENV === 'production' ? 'blog_production' : "blog_development",
+    database: process.env.NODE_ENV === 'production' ? 'blog_production' : 'blog_development',
     entities: [Post, User, Comment]
   });
 };
 
 const promise = (async function () {
+  console.log(111);
   const manager = getConnectionManager();
+  console.log('manager');
+  console.log(manager);
+  console.log(222);
   const current = manager.has('default') && manager.get('default');
-  if (current && current.isConnected) {
+  console.log(333);
+  if (current) {
+    console.log('here close');
     await current.close();
   }
-  return create();
+  console.log('here create');
+  return await create();
 })();
 
 export const getDatabaseConnection = async () => {
-  return promise;
+  console.log('get connection');
+  const aaa = await promise;
+  console.log('aaa');
+  console.log(aaa.isConnected);
+  return aaa;
 };
+*/
+
+const options = {
+  ...config,
+  host: process.env.NODE_ENV === 'production' ? 'localhost' : config.host,
+  database: process.env.NODE_ENV === 'production' ? 'blog_production' : 'blog_development',
+  entities: [Post, User, Comment]
+}
+
+function entitiesChanged(prevEntities: any[], newEntities: any[]): boolean {
+  if (prevEntities.length !== newEntities.length) return true;
+
+  for (let i = 0; i < prevEntities.length; i++) {
+    if (prevEntities[i] !== newEntities[i]) return true;
+  }
+
+  return false;
+}
+
+async function updateConnectionEntities(connection: Connection, entities: any[]) {
+  if (!entitiesChanged(connection.options.entities, entities)) return;
+
+  // @ts-ignore
+  connection.options.entities = entities;
+
+  // @ts-ignore
+  await connection.buildMetadatas();
+
+  if (connection.options.synchronize) {
+    await connection.synchronize();
+  }
+}
+
+export async function getDatabaseConnection(){
+  const connectionManager = getConnectionManager();
+
+  if (connectionManager.has('default')) {
+    const connection = connectionManager.get('default');
+
+    if (process.env.NODE_ENV !== 'production') {
+      await updateConnectionEntities(connection, options.entities);
+    }
+
+    return connection;
+  }
+
+  // @ts-ignore
+  return await connectionManager.create({ ...options }).connect();
+}
+
+/*
+export const getDatabaseConnection = async () => {
+  const connectionManager = getConnectionManager();
+  console.log('connectionManager');
+  console.log(connectionManager);
+
+  if (connectionManager.has('default')) {
+    console.log('here close--1');
+    const current = connectionManager.get('default');
+    console.log('here close--2');
+    await current.close();
+    console.log('here close--3');
+    /!*c
+    const connection = connectionManager.get('default');
+
+    if (!connection.isConnected) {
+      await connection.connect();
+    }
+
+    return connection;*!/
+  }
+  console.log('here---');
+
+  // @ts-ignore
+  const ccc = connectionManager.create({
+    ...config,
+    host: process.env.NODE_ENV === 'production' ? 'localhost' : config.host,
+    database: process.env.NODE_ENV === 'production' ? 'blog_production' : 'blog_development',
+    entities: [Post, User, Comment]
+  });
+  console.log('here---create');
+  return await ccc.connect();
+};
+*/
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import {GetServerSideProps, NextPage} from 'next';
 import {getDatabaseConnection} from 'lib/getDatabaseConnection';
-import {Post} from 'src/entity/Post';
+import {Post} from '../../src/entity/Post';
 import Link from 'next/link';
 import qs from 'qs';
 import usePager from '../../hooks/usePager';
 
 type Props = {
   posts: Post[],
-  count: number,
   perPageCount: number,
   page: number,
   totalPages: number,
 }
 
 const PostsIndex: NextPage<Props> = (props) => {
-  const {posts,page,totalPages} = props;
+  const {posts, page, totalPages} = props;
   const {pager} = usePager({
     page,
     totalPages
-  })
+  });
   return (
     <div>
       <h1>文章列表</h1>
@@ -42,10 +41,10 @@ const PostsIndex: NextPage<Props> = (props) => {
 export default PostsIndex;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const connect = await getDatabaseConnection();
   const query = qs.parse(context.req.url.split('?')[1]);  // 获取url中的参数
   const page = parseInt(query.page as string) || 1; // 如果没有page参数，默认为1
   const perPageCount = 1;
+  const connect = await getDatabaseConnection();
   const [posts, count] = await connect.manager.findAndCount(Post, {
     skip: (page - 1) * perPageCount,
     take: perPageCount
@@ -53,7 +52,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     props: {
       posts: JSON.parse(JSON.stringify(posts)),
-      count,
       perPageCount,
       page,
       totalPages: Math.ceil(count / perPageCount)
