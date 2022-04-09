de1de3d43822aab939b6f89648152bfb84f590e9
Wu <15151851516@163.com>
2022-04-09 18:46:29 +0800

    文章详情css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import Head from 'next/head';
 import React from 'react';
-import 'styles/global.scss'
-import type { AppProps /*, AppContext */ } from 'next/app'
+import 'styles/global.scss';
+import 'github-markdown-css';
+import type {AppProps /*, AppContext */} from 'next/app';
 
-export default function App({Component,pageProps}:AppProps) {
-  return <div className={"frank"}>
+export default function App({Component, pageProps}: AppProps) {
+  return <div className={'frank'}>
     <Head>
       <title>
         我的博客 - Wu
       </title>
-      <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"/>
+      <meta name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"/>
     </Head>
     <Component {...pageProps}/>
-  </div>
+  </div>;
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {GetServerSideProps, NextPage} from 'next';
 import {getDatabaseConnection} from '../../lib/getDatabaseConnection';
 import {Post} from '../../src/entity/Post';
+import {marked} from 'marked';
 
 type Props = {
   post: Post
@@ -10,11 +11,34 @@ type Props = {
 const PostsShow: NextPage<Props> = (props) => {
   const {post} = props;
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <article dangerouslySetInnerHTML={{__html: post.content}}>
-      </article>
-    </div>
+    <>
+      <div className="wrapper">
+        <h1>{post.title}</h1>
+        <article className="markdown-body" dangerouslySetInnerHTML={{__html: marked(post.content)}}>
+        </article>
+      </div>
+      <style jsx>{`
+        .wrapper {
+          padding: 16px 24px;
+        }
+
+        h1 {
+          text-align: center;
+          border-bottom: 1px solid #ccc;
+        }
+
+        .markdown-body {
+          font-size: 16px;
+          line-height: 1.5;
+          word-break: break-word;
+          word-wrap: break-word;
+          color: #24292e;
+          background-color: #fff;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+      `}</style>
+    </>
   );
 };
 
