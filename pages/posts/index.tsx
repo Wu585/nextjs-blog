028cb4fc25e0db67388b14f86ad2ff65dcc0032d
Wu <15151851516@163.com>
2022-04-09 12:54:06 +0800

    使博客列表根据用户来显示

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {GetServerSideProps, NextPage} from 'next';
+import {GetServerSideProps, GetServerSidePropsContext, NextPage} from 'next';
 import {getDatabaseConnection} from 'lib/getDatabaseConnection';
 import {Post} from '../../src/entity/Post';
 import Link from 'next/link';
 import qs from 'qs';
 import usePager from '../../hooks/usePager';
+import {withSession} from '../../lib/withSession';
 
 type Props = {
   posts: Post[],
@@ -20,34 +21,63 @@ const PostsIndex: NextPage<Props> = (props) => {
     totalPages
   });
   return (
-    <div>
-      <h1>文章列表</h1>
-      {posts.map(post =>
-        <div key={post.id}>
-          <Link href={`/posts/${post.id}`}>
-            <a>
-              {post.title}
-            </a>
-          </Link>
-        </div>
-      )}
-      <footer>
-        {pager}
-      </footer>
-    </div>
+    <>
+      <div className="posts">
+        <h1>文章列表</h1>
+        {posts.map(post =>
+          <div key={post.id} className="onePost">
+            <Link href={`/posts/${post.id}`}>
+              <a>
+                {post.title}
+              </a>
+            </Link>
+          </div>
+        )}
+        <footer>
+          {pager}
+        </footer>
+      </div>
+      <style jsx>{`
+        .posts {
+          max-width: 800px;
+          margin: 0 auto;
+          padding: 16px;
+        }
+
+        .onePost {
+          border-bottom: 1px solid #ddd;
+          padding: 8px 0;
+        }
+
+        .onePost > a {
+          border-bottom: none;
+          color: #000;
+        }
+
+        .onePost > a:hover {
+          color: #00adb5;
+        }
+      `}</style>
+    </>
   );
 };
 
 export default PostsIndex;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = withSession(async (context: GetServerSidePropsContext) => {
+  // @ts-ignore
   const query = qs.parse(context.req.url.split('?')[1]);  // 获取url中的参数
   const page = parseInt(query.page as string) || 1; // 如果没有page参数，默认为1
-  const perPageCount = 1;
+  const perPageCount = 10;
   const connect = await getDatabaseConnection();
+  // @ts-ignore
+  const user = context.req.session.get('currentUser');
   const [posts, count] = await connect.manager.findAndCount(Post, {
     skip: (page - 1) * perPageCount,
-    take: perPageCount
+    take: perPageCount,
+    where: {
+      authorId: user.id
+    }
   });
   return {
     props: {
@@ -57,4 +87,4 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       totalPages: Math.ceil(count / perPageCount)
     }
   };
-};
+});
